b6bff21260b5433b897cde81ea27a8da99c52462
Luiz Bizzio <luizrbizzio@gmail.com>
2023-06-16 22:16:18 +0100

    Add files via upload

---
+++ menus/menuMarry.js
@@ -0,0 +1,54 @@
+exports.pt_BR = (pushname, prefix) => {
+	return `💕 [ *MENU MARRY* ] 💕
+
+*${prefix}marry* <@user>
+Pede em casamento o usuário mencionado.
+
+*${prefix}am* <@user>
+Aceita o pedido de casamento de quem lhe enviou.
+
+*${prefix}couple* <@user>
+Mostra as informações sobre seu casamento.
+
+*${prefix}divorce*
+Termina o casamento atual.
+
+*_Envie ${prefix}menu para ver a lista completa de menus!_*`}
+
+
+
+exports.en_US = (pushname, prefix) => {
+	return `💕 [ *MENU MARRY* ] 💕
+
+*${prefix}marry* <@user>
+Send a marriage proposal to the mentioned user.
+
+*${prefix}am* <@user>
+Accept the marriage proposal of the one who sent you.
+
+*${prefix}couple* <@user>
+Shows information about your marriage.
+
+*${prefix}divorce*
+Ends current marriage.
+
+*_Send ${prefix}menu to see full list of menus!_*`}
+
+
+
+exports.es_ES = (pushname, prefix) => {
+	return `💕 [ *MENU MARRY* ] 💕
+
+*${prefix}marry* <@user>
+Enviar una propuesta de matrimonio al usuario mencionado.
+
+*${prefix}am* <@user>
+Acepta la propuesta de matrimonio de quien te envió.
+
+*${prefix}couple* <@user>
+Muestra información sobre su matrimonio.
+
+*${prefix}divorce*
+Termina el matrimonio actual.
+
+*_Enviar ${prefix}menú para ver la lista completa de menús!_*`}
---
+++ menus/menuXp.js
@@ -0,0 +1,99 @@
+exports.pt_BR = (pushname, prefix) => {
+	return `🧪 [ *MENU XP* ] 🧪
+
+*${prefix}xp*
+Mostra o rank de XP que o usuário possui.
+
+*${prefix}casino* <XP>
+Apostar XP no cassino.
+
+*${prefix}roulette* <XP>
+Apostar XP na roleta.
+
+*${prefix}profile*
+Mostra os dados do seu perfil.
+
+*${prefix}level*
+Gera uma imagem da sua quantia de XP.
+
+*${prefix}forceup*
+Atualiza o sistema de XP do usuário.
+
+*${prefix}players*
+Mostra os 10 melhores jogadores.
+
+*${prefix}give* <@user> + <XP>
+Transfere ao usuário mencionado, uma quantia de XP especificada.
+
+*${prefix}tier*
+Mostra todas os niveis do sistema de XP.
+
+*_Envie ${prefix}menu para ver a lista completa de menus!_*`}
+
+
+
+exports.en_US = (pushname, prefix) => {
+	return `🧪 [ *MENU XP* ] 🧪
+
+*${prefix}xp*
+Shows the XP rank the user has.
+
+*${prefix}casino* <XP>
+Bet XP at the casino.
+
+*${prefix}roulette* <XP>
+Bet XP on Roulette.
+
+*${prefix}profile*
+Shows your profile data.
+
+*${prefix}level*
+Generates an image of your XP amount.
+
+*${prefix}forceup*
+Updates the user's XP system.
+
+*${prefix}players*
+Shows the top 10 players.
+
+*${prefix}give* <@user> + <XP>
+Transfer the mentioned user a certain amount of XP.
+
+*${prefix}tier*
+Shows all existing tiers in the XP system.
+
+*_Send ${prefix}menu to see full list of menus!_*`}
+
+
+
+exports.es_ES = (pushname, prefix) => {
+	return `🧪 [ *MENU XP* ] 🧪
+
+*${prefix}xp*
+Muestra el rango de XP que tiene el usuario.
+
+*${prefix}casino* <XP>
+Apostar xp en el casino.
+
+*${prefix}roulette* <XP>
+Apostar xp en la ruleta.
+
+*${prefix}profile*
+Muestra los datos de su perfil.
+
+*${prefix}level*
+Genera una imagen de su suma de XP.
+
+*${prefix}forceup*
+Actualiza el sistema XP del usuario.
+
+*${prefix}players*
+Muestra los 10 mejores jugadores.
+
+*${prefix}give* <@user> + <XP>
+Transferir al usuario mencionado, cierta cantidad de XP.
+
+*${prefix}tier*
+Muestra todas los niveles existentes en el sistema XP.
+
+*_Enviar ${prefix}menú para ver la lista completa de menús!_*`}
