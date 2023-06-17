530dcf0de43d813c25933ca7b72b12155388f7fc
Luiz Bizzio <luizrbizzio@gmail.com>
2023-06-17 19:51:17 +0100

    Add files via upload

--- lib/installWFonts.bat
+++ lib/installWFonts.bat
@@ -1,31 +1,16 @@
-REM http://www.msfn.org/board/topic/119612-how-to-install-a-font-via-the-command-line/
-
 @ECHO OFF
 TITLE Adding Fonts..
-REM Filename: ADD_Fonts.cmd
-REM Script to ADD TrueType and OpenType Fonts for Windows
-REM By Islam Adel
-REM 2012-01-16
-
-REM How to use:
-REM Place the batch file inside the folder of the font files OR:
-REM Optional Add source folder as parameter with ending backslash and dont use quotes, spaces are allowed
-REM example "ADD_fonts.cmd" C:\Folder 1\Folder 2\
-
 IF NOT "%*"=="" SET SRC=%*
 ECHO.
 ECHO Adding Fonts..
 ECHO.
 FOR /F %%i in ('dir /b "%SRC%*.*tf"') DO CALL :FONT %%i
-REM OPTIONAL REBOOT
-REM shutdown -r -f -t 10 -c "Reboot required for Fonts installation"
 ECHO.
 ECHO Done!
 exit
 
 :FONT
 ECHO.
-REM ECHO FILE=%~f1
 SET FFILE=%~n1%~x1
 SET FNAME=%~n1
 SET FNAME=%FNAME:-= %
